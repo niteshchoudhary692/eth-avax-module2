c095d2aa323d2dbbbcac78038db56e867ed96743
niteshchoudhary692 <139580646+niteshchoudhary692@users.noreply.github.com>
2023-09-17 23:49:57 +0530

    contract.sol

--- contract.sol
+++ contract.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 contract MetamaskFunctions {
     event GreetingPrinted(address indexed sender, string greeting);
     event DonationReceived(address indexed sender, uint256 amount);
+    event DataStored(address indexed sender, string data);
+
+    string private storedData;
 
     function printGreeting(string memory greeting) external {
         emit GreetingPrinted(msg.sender, greeting);
@@ -11,5 +14,14 @@ contract MetamaskFunctions {
 
     function receiveDonation() external payable {
         emit DonationReceived(msg.sender, msg.value);
+    }
+
+    function storeData(string memory data) external {
+        storedData = data;
+        emit DataStored(msg.sender, data);
+    }
+
+    function retrieveData() external view returns (string memory) {
+        return storedData;
     }
 }
